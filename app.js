70af9de36b55d21ea150ce2eaad856a1f5f24753
hrn2k1 <Harun.Rashid@dev.dctieto.fi>
2014-04-03 11:11:07 +0600

    process.env.port

--- app.js
+++ app.js
@@ -25,4 +25,4 @@ var server = http.createServer(function (request, response) {
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8383);
+server.listen(process.env.port || 8383);
